8e9b862ca8058591dd1e4653f8226991671d2baf
gastonlopez27 <adpgastonlopez@gmail.com>
2016-09-13 21:19:21 -0300

    primer parte

---
+++ Clase 5/Clase 5/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Libreria;
+
+namespace Clase_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Banco[] _bancos = new Banco[10];
+
+            
+            Banco a = new Banco("asd", 1, 239);
+            Banco b = new Banco("afd", 1, 232);
+            Banco c = new Banco("afssd", 5, 213);
+            Banco d = new Banco("asved", 101, 123);
+            Banco k = new Banco("asd554", 154, 423);
+            Banco f = new Banco("asd1sd", 121, 243);
+            Banco g = new Banco("asdczc", 12, 234);
+            Banco h = new Banco("asvvfd", 10, 235);
+            Banco i = new Banco("aswfwd", 12111, 523);
+            Banco j = new Banco("aawwasd", 321, 253);
+
+            _bancos.SetValue(a, 0);
+            _bancos.SetValue(b, 1);
+            _bancos.SetValue(c, 2);
+            _bancos.SetValue(d, 3);
+            _bancos.SetValue(f, 4);
+            _bancos.SetValue(g, 5);
+            _bancos.SetValue(h, 6);
+            _bancos.SetValue(i, 7);
+            _bancos.SetValue(j, 8);
+            _bancos.SetValue(k, 9);
+
+            Array.Sort(_bancos, Banco.ComparacionBancos);
+
+            for (int z = 0; z < 10; z++)
+			{
+			  this.listBox1.Items.Add(_bancos[z]);
+			}
+
+            this.listBox1.SelectedItem = a;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(listBox1.SelectedItem.ToString());
+        }
+
+        
+
+        
+    }
+}
---
+++ Clase 5/Libreria/Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libreria
+{
+    public class Banco
+    {
+        private int _sucursal;
+        private string _direccion;
+        private int _altura;
+
+        public int Sucursal {
+            get { return _sucursal; }
+            set { _sucursal = value; }
+        }
+
+        public string Direccion { 
+            get { return _direccion; }
+            set { _direccion = value; }
+        }
+
+        public int Altura {
+            get { return _altura; }
+            set { _altura = value; }
+        }
+
+        public Banco(string d, int a, int s)
+        {
+            this.Altura = a;
+            this.Direccion = d;
+            this.Sucursal = s;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Sucursal: "+this._sucursal.ToString());
+            sb.Append(" Direccion: " + this._direccion);
+            sb.Append(" Altura: "+this._altura.ToString());
+            
+
+            return sb.ToString();
+
+        }
+
+        
+
+       
+        public static int ComparacionBancos(Banco a, Banco b)
+        {
+
+            if (a.Sucursal > b.Sucursal)
+                return 1;
+            else if (a.Sucursal == b.Sucursal)
+                return 0;
+            else 
+                return -1;
+
+            
+        }
+
+
+    }
+}
